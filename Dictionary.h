37da69573014e6d3aa7e2f1639a0b0137dd23f8c
Bishnu Khadka <bishnukhadka442@gmail.com>
2020-10-19 21:53:56 +0545

    Created Dictionary.h file and SpellChecker.h file
     Here we have the declaratin of all the functions we need to perform spelling checking and also correction.

---
+++ Dictionary.h
@@ -0,0 +1,25 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+#include "SpellChecker.h"
+
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Dictionary
+{
+private:
+    unordered_map<string, string> wordDictionay;
+
+    vector<string> suggestions;
+
+    SpellChecker spell_checker;
+
+public:
+    Dictionary(const string &filename);
+
+    void inputFile(const string &filename);
+};
+
+#endif
---
+++ SpellChecker.h
@@ -0,0 +1,27 @@
+
+#ifndef SPELLCHECKER_H
+#define SPELLCHECKER_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class SpellChecker
+{
+private:
+    void CreateWordList_bySwappingAdjCharacters(const string &word);
+
+    void CreateWordList_byDeletingCharacters(const string &word);
+
+    void CreateWordList_byReplacingCharacters(const string &word);
+
+    void CreateWordList_byInsertingCharacters(const string &word);
+
+public:
+    void to_lower_case(string &word);
+    void remove_unnecessary_characters(string &word);
+    bool contains(const string &word);
+    vector<string> get_top_suggestions();
+};
+
+#endif
